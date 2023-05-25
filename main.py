99612f4f1ca750bd9b78509f97ffe2fdce2b0d38
sverg84 <134344913+sverg84@users.noreply.github.com>
2023-05-25 14:16:35 -0700

    placeholder app.post

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
 from fastapi import FastAPI
 
+import schemas
+
 app = FastAPI()
 
 @app.get("/")
-def read_root():
+def read_root() -> str: 
     return "I want snax"
+
+@app.post("/url")
+def create_url(url: schemas.URLBase) -> str:
+    return url.__str__
